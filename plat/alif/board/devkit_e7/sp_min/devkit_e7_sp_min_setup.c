44e9ad883e0ad54106a82025f01a96445f00942c
Silesh C V <silesh@alifsemi.com>
2025-03-18 11:32:51 +0530

    plat: alif: Add support for is66wvh64m8dall psram
    Add support for the 64MB PSRAM on FPGA development board1. Remove
    the write streaming threshold workaround  as that is not needed
    in eagle.
    
    Signed-off-by: Silesh C V <silesh@alifsemi.com>

--- plat/alif/board/devkit_e7/sp_min/devkit_e7_sp_min_setup.c
+++ plat/alif/board/devkit_e7/sp_min/devkit_e7_sp_min_setup.c
@@ -21,32 +21,14 @@
 
 extern int init_nor_flash(void);
 
-#if HYPRAM_EN
-static void set_actlr_radis(void)
-{
-	/*
-	 * Disable streaming by setting RADIS(bits 28 and 27) and
-	 * L1RADIS(bits 26 and 25). All write-allocate lines allocate
-	 * in the L1 or L2 cache.
-	 */
-	u_register_t val = read_actlr();
-	val |= 0x1E000000;
-	write_actlr(val);
-}
-#endif
+#define PERIPH_CLK_ENA			0x4902F03C
+#define PERIPH_CLK_ENA_OSPI0_CKEN	(1 << 0)
+#define PERIPH_CLK_ENA_OSPI1_CKEN	(1 << 1)
 
 static void devkit_e7_devices_init(void)
 {
 	uint32_t value;
 
-#if HYPRAM_EN
-	/*
-	 * Disable write streaming mode by setting the RADIS bits
-	 * in the CPU ACTLR. Needed for the correct operation of
-	 * the hyperram memory.
-	 */
-	set_actlr_radis();
-#endif
 	/* Enable peripheral and APB clocks */
 	value = mmio_read_32(EXPMST0_CTRL_REG);
 	mmio_write_32(EXPMST0_CTRL_REG, (value | 0xC0000000));
@@ -54,6 +36,9 @@ static void devkit_e7_devices_init(void)
 	/* Enable UART clock and select UART clock source */
 	value = mmio_read_32(UART_CTRL_REG);
 	mmio_write_32(UART_CTRL_REG, (value | 0x0000FFFF));
+
+	value = mmio_read_32(PERIPH_CLK_ENA);
+	mmio_write_32(PERIPH_CLK_ENA, (value | PERIPH_CLK_ENA_OSPI0_CKEN));
 
 	pinconf_set(PORT_1, PIN_0, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
 	pinconf_set(PORT_1, PIN_1, PINMUX_ALTERNATE_FUNCTION_1, 0);
@@ -64,51 +49,29 @@ static ospi_cfg_t ospi_cfg;
 
 static void ospi_hyperram_init(void)
 {
-	uint32_t value;
-
-	pinconf_set(PORT_2, PIN_0, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_1, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_2, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_3, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_4, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_5, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_6, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_2, PIN_7, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_3, PIN_0, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_3, PIN_1, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_3, PIN_2, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_1, PIN_6, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE);
-	pinconf_set(PORT_15, PIN_6, PINMUX_ALTERNATE_FUNCTION_0, 0);
-
-	value = mmio_read_32((LPGPIO_BASE + GPIO_INTMASK_OFFSET));
-	mmio_write_32((LPGPIO_BASE + GPIO_INTMASK_OFFSET),
-		(value | (1 << OSPI_RESET_PIN)));
-
-	/* set direction */
-	value = mmio_read_32((LPGPIO_BASE + GPIO_SWPORTA_DDR_OFFSET));
-	mmio_write_32((LPGPIO_BASE + GPIO_SWPORTA_DDR_OFFSET),
-		(value | (1 << OSPI_RESET_PIN)));
-
-	/* set low state */
-	value = mmio_read_32((LPGPIO_BASE + GPIO_SWPORTA_DR_OFFSET));
-	mmio_write_32((LPGPIO_BASE + GPIO_SWPORTA_DR_OFFSET),
-		(value & ~(1 << OSPI_RESET_PIN)));
-
-	/* set high state */
-	value = mmio_read_32((LPGPIO_BASE + GPIO_SWPORTA_DR_OFFSET));
-	mmio_write_32((LPGPIO_BASE + GPIO_SWPORTA_DR_OFFSET),
-		(value | (1 << OSPI_RESET_PIN)));
+	pinconf_set(PORT_0, PIN_0, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_1, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_2, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_3, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_4, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_5, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_6, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_0, PIN_7, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_1, PIN_7, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_3, PIN_5, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_1, PIN_4, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
+	pinconf_set(PORT_3, PIN_4, PINMUX_ALTERNATE_FUNCTION_1, PADCTRL_READ_ENABLE | PADCTRL_OUTPUT_DRIVE_STRENGTH_12MA);
 
 	ospi_cfg_t *ospi = &ospi_cfg;
+
 	ospi->regs = (ospi_regs_t *) OSPI0_BASE;
 	ospi->aes_regs = (aes_regs_t *) AES0_BASE;
 	ospi->ss = 0;
 
 	ospi_disable(ospi);
-	writel(ospi, baudr, 0x8);
-	writel(ospi, rx_sample_dly, 0x0);
+	writel(ospi, baudr, 0x4);
+	writel(ospi, rx_sample_dly, 0x5);
 	writel(ospi, txd_drive_edge, 0x1);
-	ospi->aes_regs->aes_rxds_delay = 0x10;
 	ospi_enable(ospi);
 
         /* Set 64byte wrap length (b01) along with default settings*/
--- plat/alif/common/drivers/ospi/issi_hyperram.c
+++ plat/alif/common/drivers/ospi/issi_hyperram.c
@@ -8,7 +8,10 @@
 #include <plat/alif/common/drivers/issi_hyperram.h>
 #include <stdint.h>
 
-#define IS66_WAIT_CYCLES	6
+#define IS66_WAIT_CYCLES		6
+#define IS66_DUAL_DIE_PACKAGE		1
+#define IS66_CA_WRITE_CONF_REG0		0x60000100
+#define IS66_CA_WRITE_CONF_REG0_DIE1	0x60200100
 
 void hyper_ram_xip_init(ospi_cfg_t *ospi)
 {
@@ -16,21 +19,22 @@ void hyper_ram_xip_init(ospi_cfg_t *ospi)
 
 	ospi_disable(ospi);
 
-	writel(ospi, spi_ctrlr0, 1 << OSPI_SPI_CTRLR0_SPI_DM_EN_OFFSET);
+	writel(ospi, ctrlr0, 0xF << OSPI_CTRLR0_DFS_OFFSET);
 
-	val = (1 << OSPI_XIP_CTRL_RXDS_SIG_EN_OFFSET)
-		| (1 << OSPI_XIP_CTRL_XIP_HYPERBUS_EN_OFFSET)
-		| (IS66_WAIT_CYCLES << OSPI_XIP_CTRL_WAIT_CYCLES_OFFSET);
+	val = (IS66_WAIT_CYCLES << OSPI_XIP_CTRL_WAIT_CYCLES_OFFSET)
+		| (0x1 << OSPI_XIP_CTRL_XIP_HYPERBUS_EN_OFFSET)
+		| (0x1 << OSPI_XIP_CTRL_RXDS_SIG_EN_OFFSET)
+		| (0x1 << OSPI_XIP_CTRL_DFS_HC_OFFSET);
 
 	writel(ospi, xip_ctrl, val);
 
-	val = (1 << OSPI_XIP_WR_CTRL_WR_HYPERBUS_EN)
-		| (1 << OSPI_XIP_WR_CTRL_WR_RXDS_SIG_EN)
-		| (IS66_WAIT_CYCLES << OSPI_XIP_WR_CTRL_WAIT_CYCLES);
+	val = (IS66_WAIT_CYCLES << OSPI_XIP_WR_CTRL_WAIT_CYCLES)
+		| (0x1 << OSPI_XIP_WR_CTRL_WR_DM_EN)
+		| (0x1 << OSPI_XIP_WR_CTRL_WR_HYPERBUS_EN)
+		| (0x1 << OSPI_XIP_WR_CTRL_WR_RXDS_SIG_EN)
+		| (0x1 << OSPI_XIP_WR_CTRL_DFS_HC);
 
 	writel(ospi, xip_write_ctrl, val);
-
-	writel(ospi, xip_ser, 1 << ospi->ss);
 
 	ospi_enable(ospi);
 
@@ -70,10 +74,21 @@ void hyper_ram_write_conf_reg0(ospi_cfg_t *ospi, uint16_t data)
 	 * Form the command to write into configuration register 0.
 	 * Refer to the ISSI data sheet for the below CA bytes.
 	 */
-	writel(ospi, datareg, 0x60000100);
+	writel(ospi, datareg, IS66_CA_WRITE_CONF_REG0);
 	writel(ospi, datareg, 0x0);
 	writel(ospi, datareg, data);
 	writel(ospi, ser, 1 << ospi->ss);
 
 	while ((readl(ospi, sr) & (SR_TF_EMPTY)) != SR_TF_EMPTY);
+
+#if IS66_DUAL_DIE_PACKAGE
+	/*
+	 * write into the configuration register of the second die.
+	 */
+	writel(ospi, datareg, IS66_CA_WRITE_CONF_REG0_DIE1);
+	writel(ospi, datareg, 0x0);
+	writel(ospi, datareg, data);
+
+	while ((readl(ospi, sr) & (SR_TF_EMPTY)) != SR_TF_EMPTY);
+#endif
 }
